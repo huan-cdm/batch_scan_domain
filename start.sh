f8e5474a62d5fc26534d8a2d7635cfff93fc57b1
huan-cdm <huan151009@163.com>
2024-02-11 15:13:32 +0800

    xray报告web访问脚本迁移

--- start.sh
+++ start.sh
@@ -16,24 +16,4 @@ case "${1}" in
     # 打印出生成的文件名  
     echo "HTML output saved to $OUTPUT_FILE"
     ;;
-
-    #开启xray报告访问服务
-    #本地开启127.0.0.1，利用nginx反向代理，映射到公网访问
-    startreportserver)
-    cd /TIP/batch_scan_domain/report
-    nohup python3 -m http.server 8081 --bind 127.0.0.1 & > /TIP/batch_scan_domain/httpserver.out
-    #python3 -m http.server 8081 --bind 127.0.0.1 &
-    ;;
-
-    #关闭xray报告访问服务
-    stopreportserver)
-    pidd=`ps -aux | grep 8081 |awk -F " " '{print $2}'`
-    
-    for ii in ${pidd}
-	do
-		echo ".正在结束进程${ii}"
-		kill -9 ${ii}
-		sleep 0.1s
-	done
-    ;;
 esac
